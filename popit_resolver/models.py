ecf51e6d8e49426067e7e6fc267d3591b6ca15f1
Mark Longair <mhl@pobox.com>
2014-07-24 16:59:35 +0100

    Correct a typo in a field's help_text

--- popit_resolver/models.py
+++ popit_resolver/models.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 class EntityName(models.Model):
     name = models.TextField(db_index=True)
     start_date = models.DateField(blank=True, null=True, help_text='What date did this name/position start')
-    end_date   = models.DateField(blank=True, null=True, help_text='What date did this name/position start')
+    end_date   = models.DateField(blank=True, null=True, help_text='What date did this name/position end')
     person     = models.ForeignKey(
         Person, 
         blank=True, null=True, 
